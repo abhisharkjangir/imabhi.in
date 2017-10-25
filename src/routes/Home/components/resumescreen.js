1a55837b9fd41baadf8cf719a4898be75d8247c2
Abhsihar Jangir <abhishar.jangir@abspl.in>
2017-10-25 07:49:37 +0530

    Resume

--- src/routes/Home/components/resumescreen.js
+++ src/routes/Home/components/resumescreen.js
@@ -5,7 +5,7 @@ export const Resume = (props) => (
     <div className="container">
       <div className="row">
         <h3 className="text-center">Checkout my Resume</h3>
-        <a href='http://imabhi.in/resume.pdf' className="theme-btn-fill" download> <i className="fa fa-cloud-download" aria-hidden="true"></i> Grab a Copy</a>
+        <a href='https://s3.us-east-2.amazonaws.com/imabhi.in/resume.pdf' className="theme-btn-fill" download> <i className="fa fa-cloud-download" aria-hidden="true"></i> Grab a Copy</a>
       </div>
     </div>
   </div>
